ed390752dc08678c2351b6efd8d0cdd820599e2d
unknown <lorenzojoane@gmail.com>
2019-12-05 19:25:53 +0800

    add js file

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-<script>
-</script>
+/** load the functions after the html page is ready */
+$(document).ready(function() {
+    /** when a number is clicked, do the ff. */
+    $('.num_button').click(function() {
+    /** get the value on the display */
+        var current = $("#da_display").val();
+        /** checked if zer0 */
+            if (current == '0') {
+                current = "";
+            }
+            /**palitan ang naka-display */
+        $("#da_display").val( current + $(this) .text());
+    });
+});
